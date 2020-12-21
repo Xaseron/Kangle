728e490f243858caf6aa68ce82b444c8b2cec527
Michael Werner <xaseron@googlemail.com>
2020-12-21 15:15:51 +0100

    Update build to use Kotlin 1.4.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,22 @@
-
 plugins {
-    kotlin("js") version "1.4-M1"
+    kotlin("js") version "1.4.21"
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    maven("http://dl.bintray.com/kotlin/kotlin-eap")
     mavenCentral()
 }
 
 dependencies {
-//    compile("org.jetbrains.kotlinx:kotlinx-serialization-runtime")
     implementation(kotlin("stdlib-js"))
 }
 
 kotlin {
-    target {
-//        useCommonJs()  // this produces incompatible code
-        // both targets are needed to produce working code
-        produceExecutable()
-        nodejs { }
+    js(IR) {
+        binaries.executable()
+        browser()
     }
 }
 
